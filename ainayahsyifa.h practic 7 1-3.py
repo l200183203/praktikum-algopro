b9ee909d84586c46da20611596973396cfd589be
l200183203 <44957606+l200183203@users.noreply.github.com>
2018-11-21 21:55:11 +0700

    Add files via upload

---
+++ ainayahsyifa.h practic 7 1-3.py
@@ -0,0 +1,53 @@
+## Activity 1. Recurrence. by @AinayahSyifaHendri
+##N = {'Triangle':'L = 0.5*a*t',
+##     'Square':'L = s**2',
+##     'Rectangle':'L = p*l',
+##     'Circle':'L = pi*r**2',
+##     'Parallelogram':'L = a*t'}
+##
+##print('''
+##No| Name of Figure | broad luas
+##--|----------------|------------
+##1 | Triangle       | %s
+##2 | Square         | %s
+##3 | Rectangle      | %s
+##4 | Circle         | %s
+##5 | Parallelogram  | %s
+##'''%(N['Triangle'], N['Square'], N['Rectangle'],
+##     N['Circle'], N['Parallelogram']))
+
+## Activity 2. Password. by @AinayahSyifaHendri
+##n = 0
+##while n<3:
+##    x = input ('Enter password : ')
+##    n +=1
+##    if x=='Syifa':
+##        print ('You logged in')
+##        n=3
+##    if x!='Syifa':
+##        if n==3 :
+##            print ('sorry, you have tried 3 times. your access is denied')
+##        elif x!='Syifa' :
+##            print ('sorry, you entered the wrong password')
+
+## Activity 3. Congratulation. by @AinayahSyifaHendri
+##h = ('morning' , 'noon' , 'afternoon' , 'evening' , 'night')
+##d = input ('enter your name : ')
+##v = input('what time is it now? ')
+##v = float (v)
+##if v>=01.00 and v<=10.00:
+##    print ('good' , h[0] , d)
+##elif v>=10.01 and v<=14.59:
+##    print ('good' , h[1] , d)
+##elif v>=15.00 and v<=18.00:
+##    print ('good' , h[2] , d)
+##elif v>=18.01 and v<=20.59:
+##    print ('good' , h[3] , d)
+##elif v>=21.00 and v<=24.59:
+##    print ('good' , h[4] , d)
+
+
+    
+
+
+
