e214f3603f15e0262910c17d01d5b8675aba27d3
l200183203 <44957606+l200183203@users.noreply.github.com>
2018-11-28 21:35:28 +0700

    Add files via upload

---
+++ praktikum 8 AinayahSyifaHendri.py
@@ -0,0 +1,83 @@
+##Actvity 1. Make Module. by: Ainayah Syifa Hendri
+##d = {'Name':'Ainayah.S.H',
+##     'NIM':'L200183203',
+##     'Address':'Jendral Sudirman Street, no.85, Kota Kulon',
+##     'PostCode':'46211',
+##     'City':'Ciamis',
+##     'Province':'Jawa Barat',
+##     'Civics':'Indonesia'}
+##
+##def Name():
+##    print (d['Name'])
+##def NIM():
+##    print (d['NIM'])
+##def Address():
+##    print (d['Address'])
+##def PostCode():
+##    print (d['PostCode'])
+##def City():
+##    print (d['City'])
+##def Province():
+##    print (d['Province'])
+##def Civics():
+##    print (d['Civics'])
+##
+##def b():
+##    print ('''Options available:
+##b showing this help
+##N showing NIM
+##a showing Name
+##A showing Address
+##K showing PostCode
+##C Showing City
+##P showing Province
+##W showing Civics
+##k exit
+##''')
+##b()
+##
+##
+##while True:
+##    s = input('Your choice: ')
+##    if s == 'b':
+##        b()
+##    elif s == 'a':
+##        Name()
+##    elif s == 'N':
+##        NIM()
+##    elif s == 'A':
+##        Address()
+##    elif s == 'K':
+##        PostCode()
+##    elif s == 'C':
+##        City()
+##    elif s == 'P':
+##        Province()
+##    elif s == 'W':
+##        Civics()
+##    else:
+##        print('Thank you')
+##        break
+
+
+##Activity 2. Make a Function. by:Ainayah Syifa Hendri
+##def conversionTemperature(C = 0, F = 0):
+##    if C != 0:
+##        F = int(9/5*C+32)
+##        print ('Temperature', C, 'Celcius equivalent to', F, 'Fahrenheit')
+##    elif F != 0:
+##        C = int(5/9*(F-31))
+##        print ('Temperature', F, 'Fahrenheit equivalent to', C, 'Celcius')
+##    elif F != 0 and C != 0:
+##        F = int(9/5*C+32)
+##        print ('Temperature', C, 'Celcius equivalent to', F, 'Fahrenheit')
+##    else:
+##        F = int(9/5*C+32)
+##        print ('Temperature', C, 'Celcius equivalent to', F, 'Fahrenheit')
+
+        
+
+
+
+        
+        
