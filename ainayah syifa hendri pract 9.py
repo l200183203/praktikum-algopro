f619ff5b3ec7b324c9eabe39ac4e72bad82d38c6
l200183203 <44957606+l200183203@users.noreply.github.com>
2018-12-06 00:09:24 +0700

    Add files via upload

---
+++ ainayah syifa hendri pract 9.py
@@ -0,0 +1,26 @@
+##Activity 1. write text file. by: ainayahsyifahendri
+##file = open('L200183203', 'w')
+##file.write('L200183203' + '\n')
+##file.write('20-07-1998' + '\n')
+##file.write('Ainayah Syifa Hendri' + '\n')
+##file.close()
+
+##Activity 2. read text file. by: ainayahsyifahendri
+##NewFile = open('L200183203', 'r')
+##NIM = NewFile.readline()
+##TTL = NewFile.readline()
+##Name = NewFile.readline()
+##NewFile.close()
+##
+##import shelve
+##NewFile1 = shelve.open('Syifa.data')
+##NewFile1['Data'] = [NIM, TTL, Name]
+##NewFile1.close()
+
+##Activity 3. read shelve. by: ainayahsyifahendri
+##import shelve
+##NewFile2 = shelve.open('Syifa.data', 'r')
+##for i in NewFile2 ['Data']:
+##    print(i),
+##NewFile2.close()
+
